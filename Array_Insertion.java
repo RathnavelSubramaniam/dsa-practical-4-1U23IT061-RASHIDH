ec105c1061e7f1121688f5cc8ecd4349d75bf6ee
1U23IT061-RASHIDH <139451505+1U23IT061-RASHIDH@users.noreply.github.com>
2025-01-10 09:09:25 +0000

    done4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -20,19 +20,19 @@ public class Array_Insertion {
         e=sc.nextInt();
         arrayInsert(arr,p,e,len);
     }
-    static void arrayInsert(int arr[], int p, int e, int len) {
-        // Shift elements to the right to make space for the new element
-        for (int i = len; i >= p; i--) {
-            arr[i + 1] = arr[i]; 
+    static void arrayInsert(int[] arr,int p,int e,int len)
+    {
+    for(int i=len-1;i>=(p-1);i--)
+        {
+            arr[i+1]=arr[i];
         }
-
-        // Insert the new element at the specified position
-        arr[p] = e;
-
-        // Print the updated array
-        System.out.println("Array after insertion:");
-        for (int i = 0; i <= len; i++) {
-            System.out.print(arr[i] + " ");
+        arr[p-1]=e;
+        System.out.println("After Inserting");
+        System.out.println("Length of the array:"+(len+1));
+        for(int i=0;i<=len;i++)
+        {
+            System.out.println("arr["+i+"]="+arr[i]);
         }
     }
 }
+
